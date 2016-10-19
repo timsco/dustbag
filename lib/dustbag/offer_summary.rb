a3580d3d6b510b1697f3f917a7456544a05664b0
Tim Scollick <tscollick@inthecolorfield.com>
2016-10-19 11:06:38 -0400

    Adding 'lowest_collectible_price' to OfferSummary

--- lib/dustbag/offer_summary.rb
+++ lib/dustbag/offer_summary.rb
@@ -3,7 +3,7 @@ module Dustbag
     include Parser::Node
 
     numeric_attributes :total_new, :total_used, :total_collectible,
-                       :total_refurbished
+                       :total_refurbished, :lowest_collectible_price
 
     def lowest_new_price
       Price.parse(subnode('LowestNewPrice'))
